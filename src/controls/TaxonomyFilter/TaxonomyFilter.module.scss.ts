122cccb4bd64316585e791be0e454a88e7500e79
Matthieu LANGLOIS <matthieu.langlois@exakis-nelite.com>
2020-09-03 15:02:37 +0200

    updated gitignore

--- src/controls/TaxonomyFilter/TaxonomyFilter.module.scss.ts
+++
@@ -1,9 +0,0 @@
-/* tslint:disable */
-require("./TaxonomyFilter.module.css");
-const styles = {
-  dropdownContainer: 'dropdownContainer_839ded93',
-  container: 'container_839ded93'
-};
-
-export default styles;
-/* tslint:enable */
