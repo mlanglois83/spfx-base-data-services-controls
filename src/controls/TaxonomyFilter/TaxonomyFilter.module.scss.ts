a8d8ba9a3500cf83067169d0f737003f1d3095dc
LANGLOIS Matthieu <Matthieu.LANGLOIS-ext@ca-ts.fr>
2024-08-19 14:13:19 +0200

    fix data service version

--- src/controls/TaxonomyFilter/TaxonomyFilter.module.scss.ts
+++
@@ -1,9 +0,0 @@
-/* tslint:disable */
-require("./TaxonomyFilter.module.css");
-const styles = {
-  dropdownContainer: 'dropdownContainer_0106b0b4',
-  container: 'container_0106b0b4'
-};
-
-export default styles;
-/* tslint:enable */
